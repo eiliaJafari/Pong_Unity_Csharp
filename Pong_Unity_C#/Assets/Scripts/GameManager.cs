bfbb6eb75465d934f5ee7ba98d0bddff19e43e1a
Eilia Jafari <eilia.jafari.1999@gmail.com>
2023-03-28 14:02:03 +0330

    Add files via upload
    The Main Folder

---
+++ Pong_Unity_C#/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+    public Text PlayerScoreText;
+    public Text ComputerScoreText;
+
+    private int _playerScore;
+
+    private int _computerScore;
+
+    public void PlayerScores()
+    {
+            _playerScore++;
+            
+            this.PlayerScoreText.text = _playerScore.ToString();
+            this.ball.ResetPosition();
+            this.ball.AddStartingForce();
+    }
+
+    public void ComputerScores()
+    {
+        _computerScore++;
+        
+        this.ComputerScoreText.text = _computerScore.ToString();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
+    
+    
+    
+}
